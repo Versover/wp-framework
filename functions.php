996e87fed37073876cd3067e5d90d871559653f8
Evgeniy <wizard2014@ya.ru>
2016-04-06 23:52:21 +0300

    add post meta

--- functions.php
+++ functions.php
@@ -59,3 +59,35 @@ if ( !function_exists( 'versover_setup' ) ) {
 
 	add_action( 'after_theme_setup', 'versover_setup' );
 }
+
+/**
+ * 5. Display a meta information for a specific post
+ */
+if ( ! function_exists( 'versover_post_meta' ) ) {
+    function versover_post_meta() {
+        echo '<ul class="list-inline entry-meta">';
+
+        if ( get_post_type() === 'post' ) {
+            // if the post is sticky, mark it.
+            if ( is_sticky() ) {
+                echo '<li class="meta-featured-post"><i class="fa fa-thumb-tack"></i>' . __( 'Sticky', 'versover' ) . '</li>';
+            }
+
+            // get the post author
+            printf(
+                '<li class="meta-author"><a href="%1$s" rel="author">%2$s</a></li>',
+                esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+                get_the_author()
+            );
+
+            // get the date
+            echo '<li class="meta-date">' . get_the_date() . '</li>';
+
+            // the categories
+            $category_list = get_the_category_list( ', ' );
+            if ( $category_list ) {
+                echo '<li class="meta-categories">' . $category_list . '</li>';
+            }
+        }
+    }
+}
